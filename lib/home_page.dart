bc116df80a5a50d4be69186ce10a9dd3de2111e2
RezaAzharii <rezaazhari867@gmail.com>
2025-05-15 01:28:58 +0700

    menambahkan HomePage dengan contructor file _imagefile

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:camera_sensor/native_camera_page.dart';
+import 'package:camera_sensor/storage_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
