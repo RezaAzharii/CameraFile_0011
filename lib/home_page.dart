dc9a026528d46d635849a9d5440ccc5aa09f58d1
RezaAzharii <rezaazhari867@gmail.com>
2025-05-14 22:42:04 +0700

    menambahkan homepage dengan struktur stateleswidget

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:camera_sensor/bloc/camera_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
